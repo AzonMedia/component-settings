79c53aac7af98fea927b93d222ffa3a839140adb
Veselin Kenashkov <kenashkov@gmail.com>
2021-03-29 22:21:27 +0300

    added SettingTemporal class

---
+++ app/src/Models/SettingTemporal.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GuzabaPlatform\Settings\Models;
+
+
+use Guzaba2\Orm\Interfaces\ActiveRecordTemporalInterface;
+
+class SettingTemporal extends Setting implements ActiveRecordTemporalInterface
+{
+
+}
